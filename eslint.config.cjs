971698c8579e42146ed6d38236c8cf34e4a045e1
Ben McNicholl <git@benmcnicholl.com>
2025-12-02 12:04:13 +1100

    chore: base files for vscode extension
    Signed-off-by: Ben McNicholl <git@benmcnicholl.com>

---
+++ eslint.config.cjs
@@ -0,0 +1,26 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: ["out/**", "*.js", "**/*.js"],
+  },
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2026,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
