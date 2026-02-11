0487aeea06dd69455e5182a9cb0bee768275b7a2
Pete Tomasik <senate6268@gmail.com>
2026-02-11 10:08:06 -0500

    Fix interaction when canceling blocked builds

--- src/treeViews/nodes/buildNode.ts
+++ src/treeViews/nodes/buildNode.ts
@@ -17,7 +17,7 @@ export class BuildNode extends vscode.TreeItem {
     const iconName = this.build.blocked ? "stop-circle" : getIconForBuild(build.state);
     this.iconPath = new vscode.ThemeIcon(iconName);
     this.tooltip = this.getTooltip();
-    this.contextValue = this.build.blocked ? "build:blocked" : `build-${build.state}`;
+    this.contextValue = this.build.blocked ? `build-${build.state}:blocked` : `build-${build.state}`;
   }
 
   private static getLabel(build: Build): string {
