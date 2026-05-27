34b3cbffbb6b54057998736fc72e93736d3c7309
Pete Tomasik <senate6268@gmail.com>
2026-05-27 14:53:53 -0400

    Remove dead GraphQL extension surface

--- src/api/oauth/constants.ts
+++ src/api/oauth/constants.ts
@@ -1,7 +1,6 @@
 export const DEFAULT_CLIENT_ID = "85bcd1d92d30dcb5e074";
 export const DEFAULT_WEB_BASE_URL = "https://buildkite.com";
 export const DEFAULT_API_BASE_URL = "https://api.buildkite.com/v2";
-export const DEFAULT_GRAPHQL_URL = "https://graphql.buildkite.com/v1";
 
 // Refresh access tokens this far before expiry to absorb clock skew
 // and request latency
